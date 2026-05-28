4782221907c4326adc9fdd5922e32fa26168c90a
Neil-Gerdsen <1211999@student.roc-nijmegen.nl>
2026-05-28 11:57:54 +0200

    song class

--- classes/Song.cs
+++ classes/Song.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace oop4.classes
 {
-    internal class Song
-    {
+    //internal class Song
+    //{
+        
+        public enum Genres
+        {
+            Pop,
+            Rock,
+            Jazz,
+            Classical,
+            HipHop,
+            Electronic
+        }
+
+        public class Song : IPlayable
+        {
+            public string Title { get; set; }
+            public List<Artist> Artists { get; set; }
+            public Genres SongGenre { get; set; }
+            private int Duur { get; set; }
+
+            public Song(string title, List<Artist> artists, int duur, Genres genre)
+            {
+                Title = title;
+                Artists = artists;
+                Duur = duur;
+                SongGenre = genre;
+            }
+
+            public void Play() { }
+            public void Pause() { }
+            public void Next() { }
+            public void Stop() { }
+            public int Length => Duur;
+
+            public override string ToString()
+            {
+                return $"{Title} - {string.Join(", ", Artists)}";
+            }
+        }
     }
-}
+//}
