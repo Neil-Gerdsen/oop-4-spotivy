b47c41f267ba0ebf2da4c3bce776ae97b518b417
Ferdinand1195450 <1195450@student.roc-nijmegen.nl>
2026-06-02 15:14:46 +0200

    een ui toegevoegd:
    er zijn nog errors met favorieten toevoegen en liedjes afspelen

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using oop4.classes;
 
 
 Song song1 = new Song("Blinding Lights", 5, Genres.Pop);
-Song song2 = new Song("Starboy", 230, Genres.Pop);
+Song song2 = new Song("Starboy", 5, Genres.Pop);
 
 // 2. SongCollection aanmaken
 SongCollection collectie = new SongCollection("Mijn lijst");
@@ -16,24 +16,111 @@ collectie.Add(song2);
 Client client = new Client(new List<Song> { song1, song2 }, collectie);
 client.HuidigeCollectie = collectie;
 
-//test om te zien of favorieten werkt.
-client.AddToFavorieten(song1);
-client.AddToFavorieten(song2);
+bool running = true;
 
-client.ShowFavorieten();
-
-client.RemoveFromFavorieten(song1);
-
-client.ShowFavorieten();
-
-client.Play();   
-//client.NextSong();
-Console.WriteLine("test");
-
-// 4. Songs ophalen en tonen
-List<IPlayable> lijst = collectie.ShowPlayables();
-
-foreach (IPlayable p in lijst)
+while (running)
 {
-    Console.WriteLine(p);  // roept ToString() aan
+    Console.WriteLine("\n=== Spotify CLI ===");
+    Console.WriteLine("1. Toon alle songs");
+    Console.WriteLine("2. Voeg song toe aan favorieten");
+    Console.WriteLine("3. Toon favorieten");
+    Console.WriteLine("4. Verwijder song uit favorieten");
+    Console.WriteLine("5. Play een song");
+    Console.Write("Kies: ");
+
+    string keuze = Console.ReadLine();
+
+    switch (keuze)
+    {
+        case "1":
+            {
+                List<IPlayable> songs = collectie.ShowPlayables();
+
+                for (int i = 0; i < songs.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {songs[i]}");
+                }
+                break;
+            }
+
+
+        case "2":
+            {
+                List<IPlayable> alleSongs = collectie.ShowPlayables();
+
+                for (int i = 0; i < alleSongs.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {alleSongs[i]}");
+                }
+
+                Console.Write("Welke song wil je toevoegen? ");
+                int addIndex = int.Parse(Console.ReadLine());
+
+                client.AddToFavorieten(alleSongs[addIndex]);
+                break;
+            }
+
+
+        case "3":
+            {
+                client.ShowFavorieten();
+                break;
+            }
+
+
+        case "4":
+            {
+                List<IPlayable> favorieten = client.Favorieten.ShowPlayables();
+
+                for (int i = 0; i < favorieten.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {favorieten[i]}");
+                }
+
+                if (favorieten.Count > 0)
+                {
+                    Console.Write("Welke favoriet wil je verwijderen? ");
+                    int removeIndex = int.Parse(Console.ReadLine());
+
+                    client.RemoveFromFavorieten(favorieten[removeIndex]);
+                    break;
+                }
+                else
+
+                    Console.Write("Je hebt geen favorieten. ");
+                break;
+            }
+
+
+        case "5":
+            {
+                List<IPlayable> songs = collectie.ShowPlayables();
+
+                Console.WriteLine("Welke song wil je afspelen?");
+
+                for (int i = 0; i < songs.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {songs[i]}");
+                }
+
+                Console.Write("Kies: ");
+                int playIndex = int.Parse(Console.ReadLine());
+
+                if (playIndex >= 0 && playIndex < songs.Count)
+                {
+                    client.CurrentlyPlaying = songs[playIndex];
+                    client.Play();
+                }
+                else
+                {
+                    Console.WriteLine("Ongeldige keuze.");
+                }
+
+                break;
+            }
+
+        default:
+            Console.WriteLine("Ongeldige keuze.");
+            break;
+            }
 }
